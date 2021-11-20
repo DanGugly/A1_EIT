ec0e54327d5c747234a9e3d483d5196975b06483
DanGugly <elijahmckoy1997@hotmail.com>
2021-11-19 23:57:04 -0500

    Transition from first to second screen added using startActivity in MainActivity onResume

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private BroadcastReceiver broadcast;
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageButton = findViewById(R.id.createAccount);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        imageButton.setOnClickListener(v -> {
+            Intent newActivity = new Intent(getBaseContext(), CreateAccount.class);
+            startActivity(newActivity);
+        });
     }
 }
