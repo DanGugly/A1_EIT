7febe2cf67f1457f37359cdee18999098f138e33
DanGugly <elijahmckoy1997@hotmail.com>
2021-11-20 23:53:57 -0500

    Added repeated password validation

--- app/src/main/java/com/example/assignment1/CreateAccount.java
+++ app/src/main/java/com/example/assignment1/CreateAccount.java
@@ -8,11 +8,15 @@ import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CreateAccount extends AppCompatActivity {
 
     private EditText email, cpassword, rpassword;
     private Button next;
     private String valid_email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +62,7 @@ public class CreateAccount extends AppCompatActivity {
                 if (edt.getText().toString() == null) {
                     edt.setError("Invalid Email Address");
                     valid_email = null;
-                } else if (isEmailValid(edt.getText().toString()) == false) {
+                } else if (!isEmailValid(edt.getText().toString())) {
                     edt.setError("Invalid Email Address");
                     valid_email = null;
                 } else {
@@ -72,9 +76,67 @@ public class CreateAccount extends AppCompatActivity {
             } // end of TextWatcher (email)
         });
 
+        cpassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //Check valid password (8chars, 1 num, 1 upper & lowercase, can have special chars
+                is_pass_valid(cpassword);
+            }
+            public void is_pass_valid(EditText pw){
+                if(String.valueOf(pw.getText())==null){
+                    pw.setError("Invalid Password");
+                }else if(!validpass(pw.getText().toString())){
+                    pw.setError("Password must contain at least 8 characters, 1 upper & lowercase");
+                }else {
+                    password = pw.getText().toString();
+                }
+            }
+            public boolean validpass(final String pw){
+                Pattern pattern;
+                Matcher matcher;
+                final String PASSWORD_PATTERN = "^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[$@$!%*#?&])[A-Za-z\\\\d$@$!%*#?&]{8,}$";
+                pattern = Pattern.compile(PASSWORD_PATTERN);
+                matcher = pattern.matcher(pw);
+
+                return matcher.matches();
+            }
+        });
+
+        rpassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(rpassword.getText().toString()==null||!(cpassword.getText().toString().equals(rpassword.getText().toString()))){
+                    rpassword.setError("Passwords don't match!");
+                }else {
+
+                }
+            }
+        });
+
         if(validated()){
             //next.setHighlighted // Highlight button after validations passed
             next.setOnClickListener(v -> {
+                //Save email and password locally
 
             });
         }
